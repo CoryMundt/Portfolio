69e0c8e8fb880a45efd74665a53532f77015dc4a
CoryMundt <corymundt24@outlook.com>
2025-02-09 23:01:50 -0700

    Update BuildDataTables.py

--- Project 1/BuildDataTables.py
+++ Project 1/BuildDataTables.py
@@ -5,14 +5,14 @@ import subprocess
 import sys
 from pathlib import Path
 
-### this file is for user to create tables not inlcuded in default list
+### This file is for the user to create tables not inlcuded in default list:
 ### ["15min", "30min", "1hour", "6hour", "12hour", "1day", "7day", "14day", "30day", "60day", "90day"]
 while True:
     run = input("Would you like to create a table not included by default? Y/N: ")
     if run.upper() not in ('Y', 'N'):
         continue
     else:
-        if run == "N":
+        if run.upper() == "N":
             quit()
         else:
             break
@@ -21,15 +21,22 @@ conn = sqlite3.connect('MarketData.sqlite')
 cur = conn.cursor()
 data = readData()
 
-
+### Core code for this file, only a function to avoid duplicate big blocks of code to account for user creating multiple
+### custom tables at one time
 def createTables():
     global data
+    ### Creates an individual table within the database given the following variables:
+    ### acronym: the acronym for the crypto currency
+    ### timeunit: hour, day, min
+    ### denom: denomination of time unit e.g. 15 for 15 min if time unit is min
+    ### cursor: cursor for communiticating to database
     def createCandleTable(acronym, timeunit, denom, cursor):
         global data
         if timeunit.lower() not in ("hour", "day", "min"):
             raise ValueError("Invalid value. Allowed values are min, hour, day")
         newDataTable = MarketDataTable(acronym, timeunit, denom)
         name = newDataTable.name
+        ### to_dict converts MarketDataTable object to dict in order to store/save it within json file
         newDataTable = to_dict(newDataTable)
         key = str(denom) + timeunit
         data[acronym]["tables"][key] = newDataTable
@@ -72,38 +79,46 @@ def createTables():
             print("Error: Please enter a whole number")
             continue
     key = str(denom) + timeunit
-
+    ### Checks whether a table already exists by searching for it within the json files, if an error is given
+    ### returns 0 or -1 whether or not table exists in order to break out of the while loop and not prompt the user
+    ### if they want the same table created for other crypto if the custom table does not have valid inputs
     def checkCreate():
         try:
             from_dict(data[acronym]['tables'][key])
             print('Table already exists')
-        except:
+            return -1
+        except NameError:
             createCandleTable(acronym, timeunit, denom, cur)
+            return 0
 
     firstLoop = True
     if len(acronym) > 1:
         while True:
             if not firstLoop:
-                acronym = input("Would your like to create this same table for another crypto? If so input acronym, if not, press Enter: ")
+                acronym = input("Would you like to create this same table for another crypto? If so input acronym, if not, press Enter: ")
                 acronym = acronym.upper()
                 if len(acronym) < 1:
                     break
-            checkCreate()
+            if checkCreate() != 0:
+                break
             print("Table Created")
             firstLoop = False
     else:
+        print("Creating tables...")
         for acronym in data:
-            print("Creating tables...")
             checkCreate()
-            print("Done")
+
+        print("Done")
 def sortData():
     global data
+    ### Assigns an int value for the str variable timeunit of each tabel and returns a tuple of said int alongside
+    ### the denomination of said time unit so that the tables can easily be compared
     def sortKey(item):
-        ### MDT means MarketDataTable object
+        ### MDT = MarketDataTable object
         MDT = from_dict(item[1])
         timeunitPriority = {"min": 0, "hour": 1, "day": 2}
         return (timeunitPriority[MDT.timeunit], MDT.denomination)
-
+    ### Sorts the dictionary data by timeunit and then denomination
     for crypto in data:
         sortedTables = dict(sorted(data[crypto]["tables"].items(), key=sortKey))
         data[crypto]["tables"] = sortedTables
@@ -120,7 +135,7 @@ while True:
     else:
         print("Enter Y or N")
         continue
-
+### Sorts data with the additions created by user
 sortData()
 
 venv_python = Path(sys.executable)
